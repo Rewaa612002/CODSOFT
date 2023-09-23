0194fc5f5cc3c7c6164f39615853349494565744
Rewaa612002 <sezar.rewaa17@gmail.com>
2023-09-23 22:57:58 +0300

    Adding Portofolio all

--- Portofolio/js/script.js
+++ Portofolio/js/script.js
@@ -5,12 +5,41 @@ $(document).ready(function(){
 
         }else{
             $('.navbar').removeClass("sticky");
-
+        }
+        if(this.scrollY>500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     });
+
+    //scroll
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0})
+    });
+
+    //menu
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
 
     });
+});
+
+//typing
+var typed = new Typed(".typing",{
+    strings: ["Software Developer","Full-stack Developer", "Back-End Developer", "Front-End Developer","Game Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+});
+
+
+var typed = new Typed(".typing-2",{
+    strings: ["Software Developer","Full-stack Developer", "Back-End Developer", "Front-End Developer","Game Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
 });
